f497397a00a7a580c5626c7ed64058b17574fb44
ZahiAhmed <zahi.ahmed33@gmail.com>
2023-03-18 08:43:13 -0400

    deploy test 23

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.mount_path = nil
+  # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   config.action_cable.allowed_request_origins = [ "https://chaoslive.herokuapp.com", /https:\/\/chaoslive.herokuapp.*/ ]
 
