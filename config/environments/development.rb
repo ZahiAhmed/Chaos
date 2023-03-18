33f6060e8f8a5c798d2c56177ad0d6aed2fd353e
ZahiAhmed <zahi.ahmed33@gmail.com>
2023-03-18 08:06:18 -0400

    deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,5 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
 end
