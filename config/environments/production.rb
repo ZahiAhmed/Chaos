7d989e972919856f5aad1cfaa1b70664556b9316
ZahiAhmed <zahi.ahmed33@gmail.com>
2023-03-18 08:36:58 -0400

    deploy test 23

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://chaoslive.herokuapp.com/cable" || "ws://chaoslive.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = [/http:\/\/www\.chaoslive.herokuapp\.com/, /https:\/\/www\.chaoslive.herokuapp\.com/]
+  # config.action_cable.url = "wss://example.com/cable"
+  config.action_cable.allowed_request_origins = [ "http://chaoslive.herokuapp.com", /http:\/\/chaoslive.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
