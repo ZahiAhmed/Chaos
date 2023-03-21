6fb57c221cf654bdc8ed07b71b128f4e03699ec8
zahiahmed <zahi.ahmed33@gmail.com>
2023-03-21 11:34:12 -0400

    deploy test again

--- frontend/src/consumer.js
+++ frontend/src/consumer.js
@@ -1,7 +1,7 @@
 import { createConsumer } from "@rails/actioncable";
 
 let wsUrl;
-if (process.env.RAILS_ENV !== "production") 
+if (process.env.NODE_ENV !== "production") 
   wsUrl = "ws://localhost:5000/cable";
 else
   wsUrl = "wss://chaoslive.herokuapp.com/cable";
